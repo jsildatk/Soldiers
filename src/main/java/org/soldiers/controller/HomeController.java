f66683cfd678f438cbc88ce5149bd99be533f10b
jsildatk <jakub.sildatk@gmail.com>
2019-04-06 16:54:56 +0200

    Error page

--- src/main/java/org/soldiers/controller/HomeController.java
+++ src/main/java/org/soldiers/controller/HomeController.java
@@ -4,14 +4,16 @@ import org.soldiers.model.Soldier;
 import org.soldiers.model.User;
 import org.soldiers.repository.SoldierRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
-public class HomeController {
+public class HomeController implements ErrorController {
     @Autowired
     private SoldierRepository soldierRepository;
 
@@ -21,5 +23,16 @@ public class HomeController {
         model.addAttribute("soldiers", soldiers);
         model.addAttribute("user", new User());
         return "index";
+    }
+
+    @GetMapping("/error")
+    public String errorPage() {
+        return "error";
+    }
+
+    @Override
+    @ResponseBody
+    public String getErrorPath() {
+        return "/error";
     }
 }
