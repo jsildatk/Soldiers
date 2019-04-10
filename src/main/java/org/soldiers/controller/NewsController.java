0ae64dbdcf3e3671d8e53c2026138a627d40b559
jsildatk <jakub.sildatk@gmail.com>
2019-04-10 23:38:17 +0200

    News page

---
+++ src/main/java/org/soldiers/controller/NewsController.java
@@ -0,0 +1,44 @@
+package org.soldiers.controller;
+
+import org.soldiers.model.News;
+import org.soldiers.model.User;
+import org.soldiers.repository.NewsRepository;
+import org.soldiers.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+@RequestMapping("/news")
+public class NewsController {
+    @Autowired
+    private NewsRepository newsRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("")
+    public String newsPage(Principal principal, Model model) {
+        List<News> news = newsRepository.findAll();
+        User user = userRepository.findByUsername(principal.getName());
+        model.addAttribute("news", news);
+        model.addAttribute("user", user);
+        return "news";
+    }
+
+    @GetMapping("/{id}")
+    public String newsInfoPage(@PathVariable Long id, Principal principal, Model model) {
+        News news = newsRepository.findById(id).get();
+        User user = userRepository.findByUsername(principal.getName());
+        model.addAttribute("news", news);
+        model.addAttribute("user", user);
+        return "newsInfo";
+    }
+
+}
