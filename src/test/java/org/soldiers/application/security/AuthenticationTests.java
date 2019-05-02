6e9b169f3fd90bd6d8bedf5ad8b2209073183292
jsildatk <jakub.sildatk@gmail.com>
2019-05-02 19:00:49 +0200

    Tests

--- src/test/java/org/soldiers/application/security/AuthenticationTests.java
+++ src/test/java/org/soldiers/application/security/AuthenticationTests.java
@@ -5,12 +5,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -20,22 +22,74 @@ public class AuthenticationTests {
     private MockMvc mvc;
 
     @Test
-    public void givenRequestOnHome_shouldSucceedWith200() throws Exception {
-        mvc.perform(get("/"))
+    public void givenRequestOnHomeWithAnyUser_shouldSucceedWith200() throws Exception {
+        mvc.perform(get("/").contentType(MediaType.TEXT_HTML))
+                .andDo(print())
                 .andExpect(status().isOk());
     }
 
     @Test
-    @WithMockUser(username = "admin", password = "adminPassword", authorities = "ADMIN")
-    public void givenAuthRequestOnAdmin_shouldSucceedWith200() throws Exception {
-        mvc.perform(get("/admin"))
+    @WithMockUser(authorities = "ADMIN")
+    public void givenAuthRequestOnAdminWithAdmin_shouldSucceedWith200() throws Exception {
+        mvc.perform(get("/admin").contentType(MediaType.TEXT_HTML))
+                .andDo(print())
                 .andExpect(status().isOk());
     }
 
     @Test
-    @WithMockUser(username = "soldier", password = "soldierPassword", authorities = { "SOLDIER", "COMMANDER" })
-    public void givenAuthRequestOnAdmin_shouldReturn403() throws Exception {
-        mvc.perform(get("/admin"))
-                .andExpect(status().is(403));
+    @WithMockUser(authorities = { "SOLDIER", "COMMANDER" })
+    public void givenAuthRequestOnAdminWithSoldierOrCommander_shouldReturn403() throws Exception {
+        mvc.perform(get("/admin").contentType(MediaType.TEXT_HTML))
+                .andDo(print())
+                .andExpect(status().is(403))
+                .andExpect(forwardedUrl("/403"));
+    }
+
+    @Test
+    @WithMockUser(authorities = "ADMIN")
+    public void givenAuthRequestOnSoldierOrCommanderWithAdmin_shouldReturn403() throws Exception {
+        mvc.perform(get("/soldier").contentType(MediaType.TEXT_HTML))
+                .andDo(print())
+                .andExpect(status().is(403))
+                .andExpect(forwardedUrl("/403"));
+
+        mvc.perform(get("/commander").contentType(MediaType.TEXT_HTML))
+                .andDo(print())
+                .andExpect(status().is(403))
+                .andExpect(forwardedUrl("/403"));
+    }
+
+    @Test
+    public void givenAuthRequestOnAnyUserWithoutAuth_shouldReturn302() throws Exception {
+        mvc.perform(get("/admin").contentType(MediaType.TEXT_HTML))
+                .andDo(print())
+                .andExpect(status().is(302))
+                .andExpect(redirectedUrl("http://localhost/"));
+
+        mvc.perform(get("/commander").contentType(MediaType.TEXT_HTML))
+                .andDo(print())
+                .andExpect(status().is(302))
+                .andExpect(redirectedUrl("http://localhost/"));
+
+        mvc.perform(get("/soldier").contentType(MediaType.TEXT_HTML))
+                .andDo(print())
+                .andExpect(status().is(302))
+                .andExpect(redirectedUrl("http://localhost/"));
+    }
+
+    @Test
+    @WithMockUser(username = "soldier_test", authorities = { "COMMANDER", "SOLDIER" })
+    public void givenAuthRequestOnSettingsOrPersonalDataOrEquipmentWithCommanderOrSolider_shouldSucceedWith200() throws Exception {
+        mvc.perform(get("/settings").contentType(MediaType.TEXT_HTML))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        mvc.perform(get("/personalData").contentType(MediaType.TEXT_HTML))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        mvc.perform(get("/equipment").contentType(MediaType.TEXT_HTML))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 }
--- src/test/java/org/soldiers/application/security/LoginTests.java
+++ src/test/java/org/soldiers/application/security/LoginTests.java
@@ -12,6 +12,7 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.logout;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -20,20 +21,44 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class LoginTests {
+
     @Autowired
     private MockMvc mvc;
 
     @Test
     public void givenAdminOnLogin_shouldSucceedWithRedirectToLoginSuccess() throws Exception {
-        mvc.perform(formLogin().user("admin").password("zaq1@WSX"))
+        mvc.perform(formLogin().user("admin_test").password("zaq1@WSX"))
+                .andDo(print())
                 .andExpect(status().isFound())
                 .andExpect(redirectedUrl("/loginSuccess"))
-                .andExpect(authenticated().withAuthenticationName("admin"));
+                .andExpect(authenticated().withAuthenticationName("admin_test")
+                .withAuthorities(new AuthenticationDummy("ADMIN").getAuthorities()));
+    }
+
+    @Test
+    public void givenCommanderOnLogin_shouldSucceedWithRedirectToLoginSuccess() throws Exception {
+        mvc.perform(formLogin().user("commander_test").password("zaq1@WSX"))
+                .andDo(print())
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/loginSuccess"))
+                .andExpect(authenticated().withAuthenticationName("commander_test")
+                .withAuthorities(new AuthenticationDummy("COMMANDER").getAuthorities()));
+    }
+
+    @Test
+    public void givenSoldierOnLogin_shouldSucceedWithRedirectToLoginSuccess() throws Exception {
+        mvc.perform(formLogin().user("soldier_test").password("zaq1@WSX"))
+                .andDo(print())
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/loginSuccess"))
+                .andExpect(authenticated().withAuthenticationName("soldier_test")
+                .withAuthorities(new AuthenticationDummy("SOLDIER").getAuthorities()));
     }
 
     @Test
     public void givenInvalidUserOnLogin_shouldFailWithRedirectToLoginError() throws Exception {
         mvc.perform(formLogin().user("invalidUsername").password("invalidPassword"))
+                .andDo(print())
                 .andExpect(status().isFound())
                 .andExpect(redirectedUrl("/loginError"))
                 .andExpect(unauthenticated());
@@ -42,6 +67,7 @@ public class LoginTests {
     @Test
     public void givenLogoutRequest_shouldSucceedWithRedirectToHome() throws Exception {
         mvc.perform(logout())
+                .andDo(print())
                 .andExpect(status().isFound())
                 .andExpect(redirectedUrl("/"));
     }
